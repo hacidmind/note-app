6d83b2a847009dcc39f3fade3ea8e0b306743c35
hacidmind <abiolahafeez@yahoo.com>
2022-06-19 15:53:16 +0100

    changed app name

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
         <nav className="navbar navbar-expand-lg bg-light">
             <div className="container-fluid">
                 <Link href="/">
-                <a className="navbar-brand">Navbar</a>
+                <a className="navbar-brand">Note-App</a>
                 </Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
